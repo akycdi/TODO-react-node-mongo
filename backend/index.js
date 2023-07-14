e1500086b7c0318f5d7273653aa45795b48d8bda
Arun <arun36824@gmail.com>
2023-07-14 17:18:36 +0530

    removed html

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json());
 app.use('/user', userRoutes)
 app.use('/todo', todoRoutes)
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 const mongoose = require('mongoose')
 
